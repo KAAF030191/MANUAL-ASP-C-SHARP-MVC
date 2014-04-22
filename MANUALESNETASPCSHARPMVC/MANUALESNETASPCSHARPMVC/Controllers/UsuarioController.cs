3fe2b8b6b2af8ebe7ebf165588ec5b7ea98f39d4
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2014-04-21 19:32:10 -0500

    REQUEST FORM Y FORMCOLLECTION

---
+++ MANUALESNETASPCSHARPMVC/MANUALESNETASPCSHARPMVC/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MANUALESNETASPCSHARPMVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+        public ActionResult Registrar(FormCollection values)
+        {
+            if (Request.HttpMethod == "POST") 
+            {
+                /*string nombre = Request.Form["txtNombre"];
+
+                ViewBag.nombreRecuperado = nombre;*/
+
+                string nombre = values["txtNombre"];
+
+                ViewBag.nombreRecuperado = nombre;
+
+                return View();
+            }
+
+            return View();
+        }
+	}
+}
